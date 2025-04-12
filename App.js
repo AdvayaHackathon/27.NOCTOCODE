5357e191976c8f1efeab0093a8d23d9a0c19de2b
Sushmitha-C27 <sushmithagowda0007@gmail.com>
2025-04-12 09:15:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ExploreRituals from './pages/ExploreRituals';
+import VRTour from './pages/VRTour';
+import PlanMyJourney from './pages/PlanMyJourney';
+import GetLocalHelp from './pages/GetLocalHelp'; 
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/explore-rituals" element={<ExploreRituals />} />
+        <Route path="/vrtour" element={<VRTour />} />
+        <Route path="/plan-my-journey" element={<PlanMyJourney />} />
+        <Route path="/local-help" element={<GetLocalHelp isOpen={true} onClose={() => window.history.back()} />} />
       </Routes>
     </Router>
   );
